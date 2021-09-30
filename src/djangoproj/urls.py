f424f476cdf5e93d659a5f8bf4f711bd8d498f6e
dqgarcia <donovanquintengarcia@gmail.com>
2021-09-29 21:58:37 -0700

    Added css style sheet and about.html to django

--- src/djangoproj/urls.py
+++ src/djangoproj/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, about_view
 
 urlpatterns = [
     path('', home_view, name="Homepage"),
+    path('about/', about_view, name="About"),
     path('admin/', admin.site.urls),
 ]
