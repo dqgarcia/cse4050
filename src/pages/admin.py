50a8d928ddbed028682c6edb158fb59809e0be7c
dqgarcia <donovanquintengarcia@gmail.com>
2021-10-13 19:10:19 -0700

    Added post database

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db.models.fields import TextField
+from .models import post
 
 # Register your models here.
+
+class postAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+admin.site.register(post, postAdmin)
--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.base import Model
+
 
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=3000)
+    image = models.ImageField(upload_to = 'pages', null=True, blank = True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+
+    class Meta: 
+        verbose_name = "post"
+        verbose_name_plural = "posts"
